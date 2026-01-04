ab2b58f3177ab00eedae6a7b48652dcd954854fc
realdegrees <18383355+realdegrees@users.noreply.github.com>
2026-01-04 18:15:52 +0100

    add open-meteo-api

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 const app = {
-    init() {
-        // Initialisierung
-    },
-    
-    fetchWeather(city) {
-        // API-Call
-    },
-    
-    displayWeather(data) {
-        // UI Update
+  async init() {
+    const response = await fetch(
+      `${config.API_URL}/forecast?latitude=52.52&longitude=13.41&current_weather=true`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+
+    const data = await response.json();
+    console.log(data);
+  },
+
+  fetchWeather(city) {
+    // API-Call
+  },
+
+  displayWeather(data) {
+    // UI Update
+  },
 };
 
 // Start app when DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    app.init();
+document.addEventListener("DOMContentLoaded", () => {
+  app.init();
 });
---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const config = {
+    API_URL: 'https://api.open-meteo.com/v1',
+}
