8af2e87804694f18aab93ae9b69ecc5660e302cc
realdegrees <18383355+realdegrees@users.noreply.github.com>
2026-01-04 17:55:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO Vorgaben und Anforderungen
---
+++ js/app.js
@@ -0,0 +1,18 @@
+const app = {
+    init() {
+        // Initialisierung
+    },
+    
+    fetchWeather(city) {
+        // API-Call
+    },
+    
+    displayWeather(data) {
+        // UI Update
+    }
+};
+
+// Start app when DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    app.init();
+});
